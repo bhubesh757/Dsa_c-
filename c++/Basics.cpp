0d2e779b073f47e786ef52596eb9ef3accdb310d
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-17 18:28:11 +0530

    Create Basics.cpp

---
+++ c++/Basics.cpp
@@ -0,0 +1,40 @@
+//variables and storing the data
+   string characterName = "bhubesh";   //now name is stored in the string
+   int characAge; // now here it stored in  the int variable
+   characAge = 18;
+   
+   cout <<"there once a character named as " << characterName << endl;
+   cout << "The Age of the character is " << characAge << endl;
+   
+   //Datatypes 
+   
+  // char grade = 'A'; // wt is char 
+   //string phrase = "bhubesh cyberqode";
+   //int age = 20;
+   //double gpa = 2.4;
+   //bool isMale = true;
+   //cout << age << endl;
+   
+   //getting user input from the user;
+   // store the info / data first
+    //string name;
+    //cout<<"Enter your name : ";
+    //getline(cin,name);
+    
+    //cin >> name; 
+    //cout<<"Your name is " << name << endl;
+    
+    //cout << "you are " << age <<"Years old , Eligible for voting" << endl;
+    
+    //calculator in cpp
+    
+    int num1, num2;
+    cout << "enter the first number :" ;
+    cin>>num1;
+    
+    cout << "enter the second number :" ;
+    cin>>num2;
+    
+    
+    cout << num1+num2 << endl;
+    cout << num1*num2 << endl;
