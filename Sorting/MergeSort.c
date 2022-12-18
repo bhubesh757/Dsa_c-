e6e8c7bc29cdf7c6ece7b67854913b168ef0f50c
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-18 17:05:07 +0530

    Update MergeSort.c

--- Sorting/MergeSort.c
+++ Sorting/MergeSort.c
@@ -2,30 +2,104 @@
 
 using namespace std;
 
-void mergeSort(int[] , int , int);
-void merge(int[] , int , int);
+//creating a mergesort algorithm
 
-void printArray(int arr[] , int size) 
+//creating the merge function
+
+void merge(int arr[] , int l , int r , int m) 
 {
-    int i;
-    for(i = 0 ; i<size ; i++) {
-        count << arr[i] <<" ";
+    int i = l;
+    int j = m+1;
+    int k = l;
+    
+    int temp[5];
+    
+    //creating a while loop
+    
+    while (i <= m && j <= r) 
+    {
+        if(arr[i] <= arr[j]) 
+        {
+            temp[k] = arr[i] ;
+            i++;
+            k++;
+        }
+        else 
+        {
+            temp[k] = arr[j] ;
+            j++;
+            k++;
+            
+        }
     }
     
-    cout<< endl;
+    if(i > m) 
+    {
+        while(j <= r) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+    else 
+    {
+        {
+        while(i <= m) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+    }
+    }
+    
+    for(int s=1; s<5;s++ ) {
+        arr[s] = temp[s];
+    }
 }
 
-//it contains the array to be sorted
-int main() {
+
+
+void mergeSort(int arr[] , int l , int r ) {
     
-    int array[] = {5,4,8,9,1,2,4,5,6,7};
-    int i;
+    if(l<r) {
+        int m = (l+r) / 2;
+        
+        mergeSort(arr , l , m);
+        mergeSort(arr , m+1 , r);
+        merge(arr , l,m,r);
+        
+    }
     
-    int size = sizeof(array)/sizeof(array[0]);
-    printArray(arraay , size);
+}
+
+
+
+
+int main () 
+{
+    //inr arr[5] = {4,6,1,2,7};
+    //creating the array
+    cout<<"Enter 5 elements in the array :"<<endl; 
+    int myarr[5];
+    for(int i =0; i<5 ; i++) 
+    {
+        cin>>myarr[i];
+    }
     
-    mergeSort(array , 0 , size -1);
-    printArray(array , size);
+    cout <<"Before Merge Sort" << endl;
+    for(int i =0; i<5 ; i++) 
+    {
+        cout<<myarr[i] <<" ";
+    }
     
+    //MergeSort algorithm
+    mergeSort(myarr , 0,4);
     
+     cout<<endl <<"After Merge Sort" << endl;
+    for(int i =0; i<5 ; i++) 
+    {
+        cout<<myarr[i] <<" ";
+    }
+    
+    return 0;
 }
