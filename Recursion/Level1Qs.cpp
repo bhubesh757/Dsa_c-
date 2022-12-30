7db4e48cfdef889ab84b79763fbb483cc0424657
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-30 19:55:48 +0530

    Update Level1Qs.cpp

--- Recursion/Level1Qs.cpp
+++ Recursion/Level1Qs.cpp
@@ -96,5 +96,44 @@ int main() {
     return 0;
 }
 
+--Printing all the subsequences in the array
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//creating function for both take and not take
+
+void  f(int ind ,vector<int> &ds , int arr[] , int n )  {
+if (n == 0) {
+	for (auto it : ds) {
+		cout<< it << " " ;
+	}
+
+	if(ds.size() == 0) {
+		cout << "{ }" ;
+	}
+	cout<< endl;
+	return;
+}
+
+ds.push_back(arr[ind]);
+f(ind + 1 , ds ,arr ,n );
+ds.pop_back();
+f(ind + 1 ,ds ,  arr , n);
+    
+}
+
+
+int main() {
+
+
+	int arr[] = {3,2,1};
+	int n =3;
+	vector<int> ds;
+	f(0,ds,arr,n);
+
+	return 0;
+}
 
 
