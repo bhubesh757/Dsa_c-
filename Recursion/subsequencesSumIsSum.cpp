1accd2c8bb0c18e7aab158dbe546a968ca6ab620
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-01-02 10:29:13 +0530

    Update subsequencesSumIsSum.cpp

--- Recursion/subsequencesSumIsSum.cpp
+++ Recursion/subsequencesSumIsSum.cpp
@@ -58,3 +58,55 @@ int main() {
 
 
 }
+
+------------------------------
+#include<iostream>
+#include<bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+//count the subsequences sum of k;;
+
+int  f(int ind   , int s , int sum , int arr[] , int n )  {
+
+if(ind == n) {
+
+	if(s == sum) {
+	return 1;
+}
+else{
+return 0;
+}
+
+}
+
+
+s -= arr[ind];
+int l = f(ind+1, s , sum , arr, n);
+
+s-= arr[ind];
+
+int r =  f(ind+1  , s , sum , arr, n);
+
+return l + r;
+
+}
+
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int arr[]= {1,2,1} ;
+	int n = 3;
+	int sum = 2;
+	cout<< f(0 , 0 , sum , arr,n);
+
+	return 0;
+
+
+}
