1652b93d5f1ca8adb22d78ee9d3f804f3177cb20
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-01-24 22:08:11 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 
-void BubbleSort(int a[] , int n){
-	for(int i = 0 ;i<n-1 ; i++) {
-		for(int j = 0 ; i<n-i-1 ; j++ ){
+void BubbleSort(int a[] ){
+	for(int i = 0 ;i<5 ; i++) {
+		for(int j = 0 ; j<(5-i-1) ; j++ ){
 			if(a[j] > a[j+1] ) {
 				//swap
 				int temp = a[j];
@@ -19,25 +19,25 @@ void BubbleSort(int a[] , int n){
 int main() {
 	// initializing 
 	//int a[5] = {4, 6 ,1 ,2 7};
-	int a[5];
+	int arr[5];
 	int n ;
 	cout<<"enter 5 intergers in any order :" <<endl;
 	for(int i = 0 ; i<5 ;i++) {
-		cin>>a[i];
+		cin>>arr[i];
 	}
 
 	cout<<"Before Bubble Sort"<<endl;
 
 	for(int i = 0 ; i<5 ;i++) {
-		cout<<a[i]<<" ";
+		cout<<arr[i]<<" ";
 	}
 
-	BubbleSort(a , 5);
+	BubbleSort(arr);
 
 	cout<<"After Bubble sort"<< endl;
 
 	for(int i = 0 ; i<5 ;i++) {
-		cout<<a[i]<<" ";
+		cout<<arr[i]<<" ";
 	}
 
 
