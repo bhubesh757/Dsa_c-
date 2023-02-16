da2d073bb987ac57f6c200e9f75f3be1e760f8a4
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-02-16 12:48:02 +0530

    Update selectionSort.cpp

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -47,3 +47,51 @@ int main() {
 
 	return 0;
 }
+
+//working code
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void selectionSort(int a[] , int n) {
+	for(int i = 0 ; i <= n-2 ; i++) {
+		int min = i;
+		for(int j = i; j<= n-1 ; j++) {
+			if(a[j] <  a[min]) 
+				min = j;
+		}
+
+		//swap them
+		int temp = a[i];
+		a[i] = a[min];
+		a[min] =temp;
+	}
+}
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i = 0 ; i < n ; i++){
+		cin>>arr[i];
+	}
+
+//calling the fucntion
+	selectionSort(arr , n);
+	for(int i = 0 ; i< n ; i++) {
+		cout<<arr[i] << " " <<  endl;
+	}
+
+
+	return 0;
+}
